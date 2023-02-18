f9e05e4d04bb19ea40f81f5ec423cc702d882963
ismeoh <sdtkin1996@gmail.com>
2023-02-18 21:21:24 +0000

    Deploying to gh-pages from @ ismeoh/github-pages-src@71b8bcabaecd6249859eaa51759b4a0f2e07f6cb 🚀

--- assets/index.html-6c252c84.js
+++
@@ -1,7 +0,0 @@
-import{_ as n,X as t,Y as a,Z as s,$ as e,a0 as c}from"./framework-ca96282b.js";const r={},o=e("h2",{id:"知识体系化构建流程",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#知识体系化构建流程","aria-hidden":"true"},"#"),c(" 知识体系化构建流程")],-1),d=e("p",null,[e("img",{src:"https://cdn.jsdelivr.net/gh/ismeoh/imgchr//imgs/20230219050940.png",alt:"20230219050940"})],-1);function i(_,h){return t(),a("div",null,[o,d,s(` ## 简洁化、碎片化、结构化、体系化
-
-互联网时代，最有效的学习方式是充分利用**碎片化**的时间，有计划地把碎片化的知识体系化学习  
-
-设定学习目标和时间段(半年或一年内)，设定期望的**知识体系**  
-
-通过**碎片化**时间的学习，逐步积累起**体系化**的知识   `)])}const m=n(r,[["render",i],["__file","index.html.vue"]]);export{m as default};
